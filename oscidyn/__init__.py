ee8af01f9bc9bbfb562176dc1de29051418673db
Raymond Knetemann <raymond.knetemann@gmail.com>
2025-07-18 17:31:50 +0200

    Move oscidyn folder out of src

---
+++ oscidyn/__init__.py
@@ -0,0 +1,9 @@
+from .constants import *
+from .nonlinear_dynamics import *
+from .solver import *
+
+from .examples.models import *
+
+"""
+A Python toolkit for simulating and visualizing nonlinear oscillators using experimentally realistic setups, supporting both time- and frequency-domain analyses.
+"""
--- src/oscidyn/__init__.py
+++
@@ -1,5 +0,0 @@
-from .constants import *
-from .nonlinear_dynamics import *
-from .solver import *
-
-from .examples.models import *
