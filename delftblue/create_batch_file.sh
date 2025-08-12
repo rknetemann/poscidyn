50b79f70115345ee49d8bca8363452304cb07ee9
Raymond Knetemann <raymond.knetemann@gmail.com>
2025-08-12 09:19:57 +0200

    Update memory create batch script

--- delftblue/create_batch_file.sh
+++ delftblue/create_batch_file.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:10:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8000M
+#SBATCH --mem-per-cpu=3968M
 #SBATCH --account=education-me-msc-me
 
 cd /home/rknetemann/projects/oscidyn
