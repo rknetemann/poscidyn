c00a41a56a37c1396afbe120be2f93f34035a3dd
Raymond Knetemann <raymond.knetemann@gmail.com>
2025-09-28 16:59:49 +0200

    Update frequency sweep parameters for improved accuracy and resolution

--- tests/frequency_sweep/test_frequency_sweep.py
+++ tests/frequency_sweep/test_frequency_sweep.py
@@ -1,12 +1,12 @@
 from jax import numpy as jnp
 import oscidyn
 
-Q, omega_0, gamma = 1000.0, 1.0, 0.0009
+Q, omega_0, gamma = 100000.0, 1.0, 0.0009
 MODEL = oscidyn.BaseDuffingOscillator.from_physical_params(Q=jnp.array([Q]), gamma=jnp.array([gamma]), omega_0=jnp.array([omega_0]))
 SWEEP_DIRECTION = oscidyn.SweepDirection.FORWARD
-DRIVING_FREQUENCY = jnp.linspace(0.995, 1.005, 51)
-DRIVING_AMPLITUDE = jnp.linspace(0.1*1/Q, 2.0*1/Q, 2)
-SOLVER = oscidyn.MultipleShootingSolver(max_steps=500, m_segments=20, shooting_tolerance=1e-6, max_shooting_iterations=10, rtol=1e-6, atol=1e-7)
+DRIVING_FREQUENCY = jnp.linspace(0.996, 1.004, 201)
+DRIVING_AMPLITUDE = jnp.linspace(0.1*1/Q, 5.0*1/Q, 3)
+SOLVER = oscidyn.MultipleShootingSolver(max_steps=50, m_segments=30, shooting_tolerance=1e-6, max_shooting_iterations=10, rtol=1e-5, atol=1e-7)
 PRECISION = oscidyn.Precision.SINGLE
 
 print("Frequency sweeping: ", MODEL)
