0f806b1bcd4a6d3f6e14a2c6632c98f6d8ae35f7
Raymond Knetemann <raymond.knetemann@gmail.com>
2025-09-28 14:41:51 +0200

    Results in very nice duffing plot

--- tests/frequency_sweep/test_frequency_sweep.py
+++ tests/frequency_sweep/test_frequency_sweep.py
@@ -1,11 +1,11 @@
 from jax import numpy as jnp
 import oscidyn
 
-Q, omega_0, gamma = 1000.0, 1.0, 0.00009
+Q, omega_0, gamma = 1000.0, 1.0, 0.0009
 MODEL = oscidyn.BaseDuffingOscillator.from_physical_params(Q=jnp.array([Q]), gamma=jnp.array([gamma]), omega_0=jnp.array([omega_0]))
 SWEEP_DIRECTION = oscidyn.SweepDirection.FORWARD
 DRIVING_FREQUENCY = jnp.linspace(0.995, 1.005, 51)
-DRIVING_AMPLITUDE = jnp.linspace(0.1*1/Q, 1.0*1/Q, 2)
+DRIVING_AMPLITUDE = jnp.linspace(0.1*1/Q, 2.0*1/Q, 2)
 SOLVER = oscidyn.MultipleShootingSolver(max_steps=500, m_segments=20, shooting_tolerance=1e-6, max_shooting_iterations=10, rtol=1e-6, atol=1e-7)
 PRECISION = oscidyn.Precision.SINGLE
 
