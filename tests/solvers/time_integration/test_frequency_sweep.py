f5b920e3f21589814bff2e41be147cc16aef855b
Raymond Knetemann <raymond.knetemann@gmail.com>
2025-11-28 12:43:05 +0100

    Refactor code structure for improved readability and maintainability

--- tests/solvers/time_integration/test_frequency_sweep.py
+++ tests/solvers/time_integration/test_frequency_sweep.py
@@ -10,7 +10,7 @@ from matplotlib.lines import Line2D
 # gamma[0,0,0,0] = 0.00666
 
 # 2 modes:
-Q, omega_0, alpha, gamma = np.array([10.0, 20.0]), np.array([1.00, 2.0]), np.zeros((2,2,2)), np.zeros((2,2,2,2))
+Q, omega_0, alpha, gamma = np.array([10.0, 20.0]), np.array([1.00, 3.0]), np.zeros((2,2,2)), np.zeros((2,2,2,2))
 gamma[0,0,0,0] = 0.0267
 gamma[1,1,1,1] = 0.540
 alpha[0,0,1] = 7.48e-1
@@ -19,8 +19,8 @@ alpha[1,0,0] = 3.74e-1
 REQUIRED_FORCE = 1.0
 
 MODEL = oscidyn.BaseDuffingOscillator(Q=Q, alpha=alpha, gamma=gamma, omega_0=omega_0)
-DRIVING_FREQUENCY = np.linspace(0.1, 2.0, 200)
-DRIVING_AMPLITUDE = np.linspace(0.1 * REQUIRED_FORCE, 1.0 * REQUIRED_FORCE, 5)
+DRIVING_FREQUENCY = np.linspace(0.1, 4.0, 400)
+DRIVING_AMPLITUDE = np.linspace(0.1 * REQUIRED_FORCE, 1.0 * REQUIRED_FORCE, 10)
 EXCITOR = oscidyn.OneToneExcitation(drive_frequencies=DRIVING_FREQUENCY, drive_amplitudes=DRIVING_AMPLITUDE, modal_forces=np.array([1.0, 1.0]))
 MULTISTART = oscidyn.LinearResponseMultistart(init_cond_shape=(5, 5), linear_response_factor=1.0)
 SOLVER = oscidyn.TimeIntegrationSolver(max_steps=4096*1, n_time_steps=50, verbose=True, throw=False, rtol=1e-3, atol=1e-7)
