279e40060fb7e8cb4296d3faadc942aa34bdf93a
Raymond Knetemann <raymond.knetemann@gmail.com>
2026-01-06 16:19:46 +0100

    Rename project from oscidyn to poscidyn and update repository URL

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="oscidyn",
+    name="poscidyn",
     version="0.1.0",
     description="Identification and simulation of nonlinear oscillators dynamics.",
     author="Raymond Knetemann",
     author_email="rknetemann@student.tudelft.nl",
-    url="https://github.com/rknetemann/oscidyn",
+    url="https://github.com/rknetemann/poscidyn",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
